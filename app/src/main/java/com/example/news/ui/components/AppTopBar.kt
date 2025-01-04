cf98cef735b263bd82b03808ec235b3fa402e8c6
João Ponte <joao.ponte@pluricosmetica.com>
2025-01-04 02:47:56 +0000

    * v0.9

--- app/src/main/java/com/example/news/ui/components/AppTopBar.kt
+++ app/src/main/java/com/example/news/ui/components/AppTopBar.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
